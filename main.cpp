6514faa2837d70edbbf06c82cd40c6acfc852fbe
OKLng <onkhoaluong2k7@gmail.com>
2026-04-04 22:31:16 +0700

    thay linklist co ban bang vector

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <fstream>
 #include <ctime>
+#include <vector>
 using namespace std;
 
 
@@ -63,84 +64,46 @@ public:
     }
 };
 
-// Một node của linklist
-struct Node{
-    Item data;
-    Node* next;
-    Node(Item item) : data(item), next(nullptr){}
-};
-
 // Danh sách gọi món của khách hàng
 class Order {
 private:
-    Node* head;
+    vector<Item> order;
 public:
-    Order(){head = nullptr;}
+    Order() {};
     // append a food to order
     void AddFood(Item Food){
-        Node* temp = head;
         // check this food existed yet
-        while (temp != nullptr){
-            if (temp->data.GetID() == Food.GetID()){
-                temp->data.IncreaseFood();
+        for (Item& temp: order){
+            if (temp.GetID() == Food.GetID()){
+                temp.IncreaseFood();
                 return;
             }
-            temp = temp->next;
         }
-        //In case the new food
-        Node* NewFood = new Node(Food);
-        NewFood->next = head;
-        head = NewFood;
+        order.push_back(Food);
     }
     // giam/ xoa mon an trong bill
     void RemoveFood(int Id){
-        if (head == nullptr) return;
+        if (order.empty()) return;
         // Truong hop mon an nam ngay o dau
-        if (head->data.GetID() == Id){
-            if (head->data.GetSoluong() > 1){
-                head->data.DecreaseFood();
-            }else{
-                Node* temp = head;
-                head = head->next;
-                delete temp;
+        for (auto temp = order.begin(); temp != order.end(); temp++){
+            if (temp->GetID() == Id){
+                if (temp->GetSoluong() > 1){
+                    temp->DecreaseFood();
+                } else order.erase(temp);
+                return;
             }
         }
-        // Truong hop mon an nam o giua
-        Node* temp = head;
-        Node* prev = nullptr;
-        while (temp != nullptr && temp->data.GetID() != Id){
-            prev = temp;
-            temp = temp->next;
-        }
-
-        if (temp != nullptr){
-            if (temp->data.GetSoluong() > 1){
-                temp->data.DecreaseFood();
-            } else {
-                prev->next = temp->next;
-                delete temp;
-            }
-        }else{
-            cout << "Khong tim thay mon an co ID: " << Id << " de xoa!" << endl; 
-        }
-
-        return;
+        cout << "Khong tim thay mon an co ID: " << Id << " de xoa!" << endl; 
     }
     // Calculate the bill
     long TotalBill(){
         long total = 0;
-        Node* temp = head;
-
-        while( temp != nullptr ){
-            total += temp->data.Value();
-            temp = temp->next;
-        }
-        
+        for (Item& temp: order) total += temp.Value();
         return total;
     }
     // In hoa don
     void PrintBill(){
-        if (head == nullptr){
+        if (order.empty()){
             cout << "Order dang trong!!" << endl;
             return;
         }
@@ -155,11 +118,9 @@ public:
              << setw(15) << "Thanh Tien" << endl;
         cout << "-----------------------------------------------------------" << endl;
         
-        Node* temp = head;
-        while (temp != nullptr){
-            temp->data.ShowFood();
-            temp = temp->next;
-        }
+        
+        for(Item& temp: order) temp.ShowFood();
+        
         cout << "-----------------------------------------------------------" << endl;
         cout << right << setw(45) << "TONG CONG: " << TotalBill() << " VND" << endl;
         cout << "===========================================================" << endl;
@@ -173,13 +134,11 @@ public:
         file << "================ HOA DON KHACH HANG ================\n";
         file << left << setw(5) << "ID" << setw(20) << "Ten" << setw(8) << "SL" << setw(12) << "ThanhTien"<<endl;
 
-        Node* temp = head;
-        while(temp != nullptr){
-            file << left << setw(5) << temp->data.GetID() 
-                << setw(20) << temp->data.GetTen() 
-                << setw(8) << temp->data.GetSoluong()
-                << setw(12) << temp->data.Value() << endl;
-            temp = temp->next;
+        for (Item& temp: order){
+            file << left << setw(5) << temp.GetID() 
+                << setw(20) << temp.GetTen() 
+                << setw(8) << temp.GetSoluong()
+                << setw(12) << temp.Value() << endl;
         }
         file << "-----------------------------------------------------------" << endl;
         file << right << setw(45) << "TONG CONG: " << TotalBill() << " VND" << endl;
@@ -191,40 +150,21 @@ public:
 
     // Delete the bill
     void ResetOrder(){
-        while (head != nullptr){
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
-    // destroy class
-    ~Order(){
-        while (head != nullptr){
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
+        order.clear();
     }
 };
 // Menu cua nha hang
 class Menu{
 private:
-    Node* head;
+    vector<Item> menu;
 public:
-    Menu(){head = nullptr;}
-    
+    Menu(){};
     // Them mon an moi vao menu
     void AddtoMenu(Item newItem){
-        Node* temp = head;
-        while(temp != nullptr ){
-            if (temp->data.GetID() == newItem.GetID()) return;
-            temp = temp->next;
-        }
+        for (Item &temp: menu) 
+            if (temp.GetID() == newItem.GetID()) return;
         
-        Node* newNode = new Node(newItem);
-        newNode->next = head;
-        head = newNode;
-
+        menu.push_back(newItem);
         return;
     }
     //Load du lieu menu tu file.
@@ -252,12 +192,7 @@ public:
         cout << left << setw(5) << "ID" 
             << setw(20) << "Ten Mon" 
             << setw(15) << "Don Gia" << endl;
-        Node* temp = head;
-        while( temp != nullptr){
-            temp->data.ShowFoodinMenu();
-            temp = temp->next;
-        }
-        return;
+        for (Item &temp: menu) temp.ShowFoodinMenu();
     }
     // Admin them mon moi vao data
     void Admin_themon(){
@@ -273,7 +208,7 @@ public:
         Item newItem(id, name, gia);
         AddtoMenu(newItem);
         // them vao menu trong data
-        SaveMenutoFile("Menu.txt");
+        SaveMenutoFile("L:\\Luong\\TotalBill\\Menu.txt");
         cout << " Them mon thanh cong! " << endl;
     }
     // luu du lieu vao file menu.txt
@@ -281,27 +216,20 @@ public:
         ofstream file(filename);
         if (!file.is_open()) return;
 
-        Node* temp = head;
-        while( temp != nullptr){
+        for (Item &temp: menu){
             // ghi file theo dinh dang
-            file << temp->data.GetID() << " "
-            << temp->data.GetTen() << " "
-            << temp->data.GetGia() << endl;
-            temp = temp->next;
+            file << temp.GetID() << " "
+            << temp.GetTen() << " "
+            << temp.GetGia() << endl;
         }
         file.close();
     }
     
     // Tim mon an theo ID
     Item* GetItembyId(int id){
-        Node* temp = head;
-
-        while (temp != nullptr && temp->data.GetID() != id){
-            temp = temp->next;
-        }
-
-        if (temp == nullptr) return nullptr;
-        return &(temp->data);
+        for (Item &temp: menu) 
+            if (temp.GetID() == id) return &temp;
+        return nullptr;
     }
 };
 
